00fd2ec2534fc027bcdfa85323034041a6e647b4
ExileAS <ahmedsamy0078@gmail.com>
2023-12-10 15:38:58 +0200

    ..

---
+++ client/src/features/utils/exponentialBackoff.js
@@ -0,0 +1,29 @@
+const exponentialBackoff = async (cbPromise) => {
+  let retries = 0;
+  const maxRetries = 3;
+
+  while (retries < maxRetries) {
+    try {
+      const requestResult = await cbPromise();
+      if (requestResult.success) {
+        console.log("request processed successfully!");
+        break;
+      } else {
+        throw new Error("request failed");
+      }
+    } catch (error) {
+      retries++;
+      console.error(
+        `Error during order processing attempt ${retries}: ${error.message}`
+      );
+      const delay = Math.pow(2, retries - 1) * 3000;
+      await new Promise((resolve) => setTimeout(resolve, delay));
+    }
+  }
+
+  if (retries === maxRetries) {
+    console.error("Max retries reached. Unable to process request.");
+  }
+};
+
+export default exponentialBackoff;
