b0c4e3f1e4ea28b17f92d447a33f5aedf7ee69c5
ExileAS <ahmedsamy0078@gmail.com>
2023-12-25 06:22:12 +0200

    ..

--- api/middleware/rateLimiter.js
+++ api/middleware/rateLimiter.js
@@ -1,9 +1,16 @@
 const rateLimit = require("express-rate-limit");
 
+const getKey = (req, res) => {
+  return req.ip;
+};
+
 const limiter = rateLimit({
   windowMs: 60 * 60 * 1000,
-  max: 4,
+  max: 3,
+  keyGenerator: getKey,
   handler: (req, res) => {
+    limiter.windowMs *= 2;
+
     res.status(429).json({
       errors: {
         email: "Too many attempts please try again later.",
