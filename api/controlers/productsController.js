ea06efbb971166c49f757003afe07077a5b0011e
ExileAS <ahmedsamy0078@gmail.com>
2024-01-03 20:47:15 +0200

    ..

--- api/controlers/productsController.js
+++ api/controlers/productsController.js
@@ -1,7 +1,7 @@
 const { ProductModel } = require("../models/productModel");
 
 module.exports.product_get = async (req, res) => {
-  console.log(req.body);
+  console.log("request recieved");
   try {
     const result = await ProductModel.find().sort({ createdAt: -1 });
     res.status(200).json({ result });
