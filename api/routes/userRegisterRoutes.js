84cadac75bbb686d94813b83c32a759bab24d4df
ExileAS <ahmedsamy0078@gmail.com>
2024-01-04 11:31:41 +0200

    ..

--- api/routes/userRegisterRoutes.js
+++ api/routes/userRegisterRoutes.js
@@ -10,7 +10,19 @@ const { clearTokens } = require("../middleware/tokenClearer");
 const router = Router();
 
 router.post("/api/signup", csrfProtection, clearTokens, signup_post);
-router.post("/api/login", csrfProtection, limiter, clearTokens, login_post);
+router.post(
+  "/api/login",
+  (req, res, next) => {
+    res.on("finish", () => {
+      console.log("Response headers:", res.getHeaders());
+    });
+    next();
+  },
+  csrfProtection,
+  limiter,
+  clearTokens,
+  login_post
+);
 router.get("/api/logout", logout_get);
 
 module.exports = router;
